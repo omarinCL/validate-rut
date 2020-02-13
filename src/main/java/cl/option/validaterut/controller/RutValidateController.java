a0bc4273e62ac052add41cfe51cf032f1444b801
Orlando Marin <orlando.marin@outlook.cl>
2020-02-12 23:43:48 -0300

    CORS

--- src/main/java/cl/option/validaterut/controller/RutValidateController.java
+++ src/main/java/cl/option/validaterut/controller/RutValidateController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,7 @@ import cl.option.validaterut.service.RutValidation;
 @RestController
 @RequestMapping("/api/v1/rut")
 @Validated
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET}) 
 public class RutValidateController {
     @Autowired
     private RutValidation service;
